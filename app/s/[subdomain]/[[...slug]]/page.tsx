ee08a9900eaa7d0c1f4e5d62901dfaeb6b342371
Simon Hudson <hellosimonhudson@gmail.com>
2026-01-02 10:59:24 +0000

    remove emoji

--- app/s/[subdomain]/[[...slug]]/page.tsx
+++ app/s/[subdomain]/[[...slug]]/page.tsx
@@ -52,7 +52,6 @@ export default async function Page({
 
         <div className="flex-1 flex items-center justify-center">
           <div className="text-center">
-            <div className="text-9xl mb-6">{subdomainData.emoji}</div>
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">
               {subdomain}
             </h1>
